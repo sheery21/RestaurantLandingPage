475e5fb9b6a6105790433665e82fc629da8a7fa2
sheharyar Hussian <sherypagal123@gmail.com>
2026-03-24 17:23:16 +0500

    fresh start

---
+++ src/components/About.tsx
@@ -0,0 +1,19 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+          Full Stack & Mobile App Developer with 1+ year of experience in
+          React.js, Node.js, and Flutter. Skilled in building scalable web and
+          mobile applications, integrating APIs, and creating responsive user
+          interfaces with clean, efficient, and production-ready code.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
